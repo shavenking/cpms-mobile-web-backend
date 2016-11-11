611c4f1a46f36e7cb9e2b0b05640c54bff6d9234
shavenking <shavenking@gmail.com>
2016-11-11 15:34:59 +0800

    新增登入、註冊功能

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            $token = JWTAuth::attempt($credentials);
+
+            if (!$token) {
+                return response()->json([], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json([], 500);
+        }
+
+        return response()->json(compact('token'));
+    }
+
+    public function register(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $user = $this->create($request->only('name', 'email', 'password'));
+
+        try {
+            $token = JWTAuth::attempt($credentials);
+
+            if (!$token) {
+                return response()->json([], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json([], 500);
+        }
+
+        return response()->json(compact('token'));
+    }
+
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+}
